7d6b98b8b03a5f4ac790c14aca9008c9dc9501c5
kyles-y <kyles@wcymca.org>
2025-05-03 19:11:29 -0400

    Update chat-widget.js
    fixing widget size on mobile/desktop to match accessibility widget

--- chat-widget.js
+++ chat-widget.js
@@ -5,8 +5,8 @@
       position: fixed;
       bottom: 20px;
       right: 20px;
-      width: 65px;
-      height: 65px;
+      width: 45px;
+      height: 45px;
       background-color: #20BDBE;
       border-radius: 50%;
       box-shadow: 0 2px 6px rgba(0,0,0,0.25);
@@ -19,11 +19,21 @@
       transition: opacity 1s ease;
     }
     #sms-bubble img {
-      width: 18px;
-      height: 18px;
+      width: 35px;
+      height: 35px;
     }
     #sms-bubble.show {
       opacity: 1;
+    }
+    @media (max-width: 600px) {
+      #sms-bubble {
+        width: 65px;
+        height: 65px;
+      }
+      #sms-bubble img {
+        width: 55px;
+        height: 55px;
+      }
     }
   `;
 
@@ -32,7 +42,7 @@
   link.setAttribute('aria-label', 'Contact the YMCA');
 
   const icon = document.createElement('img');
-  icon.src = 'https://thechurchco-production.s3.amazonaws.com/uploads/sites/1717/2025/05/question-v2.png';
+  icon.src = 'https://thechurchco-production.s3.amazonaws.com/uploads/sites/1717/2025/05/question.png';
   icon.alt = 'Text Us';
   link.appendChild(icon);
 
@@ -40,7 +50,10 @@
   link.href = isMobile
     ? 'sms:+18554878041?body=Replace%20this%20text%20with%20your%20question%20and%20send%20away%20to%20the%20YMCA%20%F0%9F%A4%A9'
     : 'https://forms.fillout.com/t/eZLjjVsMS3us';
-  if (!isMobile) link.target = '_blank';
+
+  if (!isMobile) {
+    link.target = '_blank';
+  }
 
   document.head.appendChild(style);
   document.body.appendChild(link);
