2393a531879538367289b737af1214a991778b13
kyles-y <kyles@wcymca.org>
2025-05-03 21:33:42 -0400

    Update chat-widget.js
    removed mobile text option and replaced with fillout form link

--- chat-widget.js
+++ chat-widget.js
@@ -5,8 +5,8 @@
       position: fixed;
       bottom: 20px;
       right: 20px;
-      width: 45px;
-      height: 45px;
+      width: 32.5px;
+      height: 32.5px;
       background-color: #20BDBE;
       border-radius: 50%;
       box-shadow: 0 2px 6px rgba(0,0,0,0.25);
@@ -19,20 +19,20 @@
       transition: opacity 1s ease;
     }
     #sms-bubble img {
-      width: 35px;
-      height: 35px;
+      width: 18px;
+      height: 18px;
     }
     #sms-bubble.show {
       opacity: 1;
     }
     @media (max-width: 600px) {
       #sms-bubble {
-        width: 65px;
-        height: 65px;
+        width: 40px;
+        height: 40px;
       }
       #sms-bubble img {
-        width: 55px;
-        height: 55px;
+        width: 24px;
+        height: 24px;
       }
     }
   `;
@@ -40,20 +40,13 @@
   const link = document.createElement('a');
   link.id = 'sms-bubble';
   link.setAttribute('aria-label', 'Contact the YMCA');
+  link.href = 'https://forms.fillout.com/t/eZLjjVsMS3us';
+  link.target = '_blank';
 
   const icon = document.createElement('img');
   icon.src = 'https://thechurchco-production.s3.amazonaws.com/uploads/sites/1717/2025/05/question.png';
   icon.alt = 'Text Us';
   link.appendChild(icon);
-
-  const isMobile = /Mobi|Android|iPhone/i.test(navigator.userAgent);
-  link.href = isMobile
-    ? 'sms:+18554878041?body=Replace%20this%20text%20with%20your%20question%20and%20send%20away%20to%20the%20YMCA%20%F0%9F%A4%A9'
-    : 'https://forms.fillout.com/t/eZLjjVsMS3us';
-
-  if (!isMobile) {
-    link.target = '_blank';
-  }
 
   document.head.appendChild(style);
   document.body.appendChild(link);
